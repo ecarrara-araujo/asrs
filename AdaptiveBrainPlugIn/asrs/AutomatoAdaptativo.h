3b97a45dbc9005762cde72eda9fbfc666db22227
ecarrara.araujo <ecarrara.araujo@043b3b24-93a8-11dd-8dfb-5da4252b82fb>
2008-10-15 03:47:06 +0000

    Start of planned behavior development.

--- AdaptiveBrainPlugIn/asrs/AutomatoAdaptativo.h
+++ AdaptiveBrainPlugIn/asrs/AutomatoAdaptativo.h
@@ -3,18 +3,18 @@
 
 //Queries pre-definidas para criacao da base utilizada pelo Automato Adaptativo.
 #define QUERY_TBL_AUTOMATO    "CREATE TABLE Automato(IdRegraPlano INTEGER NOT NULL,	PRIMARY KEY (IdRegraPlano)  FOREIGN KEY (IdRegraPlano) REFERENCES Plano (IdRegraPlano));"
-#define QUERY_TBL_PLANO       "CREATE TABLE Plano(IdRegraPlano INTEGER NOT NULL, IdEstadoInicial INTEGER NOT NULL,IdCondicao VARCHAR(2) NOT NULL,IdEstadoFinal INTEGER NOT NULL,RobotPlan INTEGER NOT NULL,Tipo CHAR(1) NOT NULL, PRIMARY KEY (IdRegraPlano) FOREIGN KEY (IdCondicao) REFERENCES Condicoes (CondId) FOREIGN KEY (IdEstadoFinal) REFERENCES Estados (EstadoId) FOREIGN KEY (IdEstadoInicial) REFERENCES Estados (EstadoId));"
-#define QUERY_TBL_ESTADOS     "CREATE TABLE Estados(EstadoId INTEGER NOT NULL,	PRIMARY KEY (EstadoId));"
+#define QUERY_TBL_PLANO       "CREATE TABLE Plano(IdRegraPlano INTEGER NOT NULL, IdEstadoInicial INTEGER NOT NULL,IdCondicao VARCHAR(2) NOT NULL,IdEstadoFinal INTEGER NOT NULL,Tipo CHAR(1) NOT NULL, PRIMARY KEY (IdRegraPlano) FOREIGN KEY (IdCondicao) REFERENCES Condicoes (CondId) FOREIGN KEY (IdEstadoFinal) REFERENCES Estados (EstadoId) FOREIGN KEY (IdEstadoInicial) REFERENCES Estados (EstadoId));" //#ECa#Campo RobotPlan transferido para a tabela de Estados.
+#define QUERY_TBL_ESTADOS     "CREATE TABLE Estados(EstadoId INTEGER NOT NULL,RobotPlan INTEGER NOT NULL,	PRIMARY KEY (EstadoId));" //#ECa#Campo RobotPlan transferido para a tabela de Estados.
 #define QUERY_TBL_CONDICOES   "CREATE TABLE Condicoes(CondId VARCHAR(2) NOT NULL,MascaraCondicao VARCHAR(32) NOT NULL,PRIMARY KEY (CondId),UNIQUE (MascaraCondicao));"
 
 //Queries para consulta a base de cohecimento.
-#define QUERY_TRANSITAR                 "SELECT Plano.IdEstadoFinal AS EstadoFinal, Plano.RobotPlan AS RobotPlan FROM Plano INNER JOIN Automato ON Automato.IdRegraPlano=Plano.IdRegraPlano INNER JOIN Condicoes ON Plano.IdCondicao=Condicoes.CondId WHERE "
+#define QUERY_TRANSITAR                 "SELECT Plano.IdEstadoFinal AS EstadoFinal, Estados.RobotPlan AS RobotPlan FROM Plano INNER JOIN Automato ON Automato.IdRegraPlano=Plano.IdRegraPlano INNER JOIN Condicoes ON Plano.IdCondicao=Condicoes.CondId INNER JOIN Estados ON Plano.IdEstadoFinal=Estados.EstadoId WHERE "
 #define QUERY_CONSULTAR_ADAPTACAO       "SELECT Plano.Tipo AS TipoEstado FROM Plano INNER JOIN Condicoes ON Plano.IdCondicao=Condicoes.CondId WHERE "
 #define QUERY_ADAPTAR_INCLUIR_TIPO      "INSERT INTO Automato(IdRegraPlano) SELECT Plano.IdRegraPlano FROM Plano WHERE "
 #define QUERY_ADAPTAR_EXCLUIR_TIPO      "DELETE FROM Automato WHERE IdRegraPlano IN (SELECT IdRegraPlano FROM Plano WHERE "
 
 #define GRUPO_ADAPTATIVO_INICIAL        "i"
-#define CONDICAO_PARA_ESTADO_INICIAL    "0001"
+#define CONDICAO_PARA_ESTADO_INICIAL    "0000000000000000"
 #define ESTADO_INICIAL                  "0"
 
 #define COLUNA_GRUPO_ADAPTACAO          "TipoEstado"
